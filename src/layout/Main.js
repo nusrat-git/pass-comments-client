312c75212adc7b742f9fc71a7479e50ed96e60f5
nusrat-git <nusratjahanmithila66@gmail.com>
2022-11-08 18:11:30 +0600

    footer and header added in different component

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Footer from '../components/Footer/Footer';
-import Header from '../components/Header/Header';
+import Footer from '../components/Shared/Footer/Footer';
+import Header from '../components/Shared/Header/Header';
+
 
 const Main = () => {
     return (
